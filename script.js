651861932c0214c4ae06edd13375ef073319cd21
ojou-less <mark.fischer0404@gmail.com>
2025-01-17 20:59:09 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById("downloadButton").addEventListener("click", function () {
+    
+    //audio file path
+    const audioPath = "audio/radiospot.mp3";
+  
+    //anchor tag
+    const link = document.createElement("a");
+    link.href = audioPath;
+  
+    //file name
+    link.download = "radiospot.mp3";
+  
+    link.click();
+  });
+
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault(); //verhindert seitenreload
+  
+    //form data
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+  
+    //sind alle felder ausgefüllt?
+    if (name && email && message) {
+      document.getElementById("contactForm").reset();
+    } else {
+      const formMessage = document.getElementById("formMessage");
+      formMessage.textContent = "Bitte fülle alle Felder aus.";
+      formMessage.classList.remove("hidden");
+      formMessage.style.color = "red";
+    }
+  });
+  
+  
